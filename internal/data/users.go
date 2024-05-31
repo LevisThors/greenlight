6df92a60e5967a22aaae2c3c176fb7a5878b3c6f
Shukri Mamatsashvili <shukri.mamatsashvili@flatrocktech.com>
2024-05-31 16:19:09 +0400

    Day 8

--- internal/data/users.go
+++ internal/data/users.go
@@ -13,6 +13,7 @@ import (
 
 var (
 	ErrDuplicateEmail = errors.New("duplicate email")
+	AnonymousUser     = &User{}
 )
 
 type User struct {
@@ -58,6 +59,10 @@ func ValidateUser(v *validator.Validator, user *User) {
 	if user.Password.hash == nil {
 		panic("missing password hash for user")
 	}
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (p *password) Set(plaintextPassword string) error {
